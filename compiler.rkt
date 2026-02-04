426f4bb68a00b3a35fd0a82bc52e46824565a838
Hari Shankar <71510856+HariShankar08@users.noreply.github.com>
2026-02-04 18:14:10 +0530

    Complete prelude-and-conclusion

--- compiler.rkt
+++ compiler.rkt
@@ -228,7 +228,23 @@
 
 ;; prelude-and-conclusion : x86int -> x86int
 (define (prelude-and-conclusion p)
-  (error "TODO: code goes here (prelude-and-conclusion)"))
+  (match p
+    [(X86Program info blocks)
+     (define stack-size (align (dict-ref info 'stack-space 0) 16))
+     (define main-prelude
+       (list (Instr 'pushq (list (Reg 'rbp)))
+             (Instr 'movq (list (Reg 'rsp) (Reg 'rbp)))
+             (Instr 'subq (list (Imm stack-size) (Reg 'rsp)))
+             (Jmp 'start)))
+     (define conclusion
+       (list (Instr 'addq (list (Imm stack-size) (Reg 'rsp)))
+             (Instr 'popq (list (Reg 'rbp)))
+             (Retq)))
+     (define new-blocks
+       (append (list (cons 'main (Block '() main-prelude))
+                     (cons 'conclusion (Block '() conclusion)))
+               blocks))
+     (X86Program info new-blocks)]))
 
 ;; Define the compiler passes to be used by interp-tests and the grader
 ;; Note that your compiler file (the file that defines the passes)
@@ -242,5 +258,5 @@
      ("instruction selection" ,select-instructions ,interp-pseudo-x86-0)
      ("assign homes" ,assign-homes ,interp-x86-0)
      ("patch instructions" ,patch-instructions ,interp-x86-0)
-     ;; ("prelude-and-conclusion" ,prelude-and-conclusion ,interp-x86-0)
+     ("prelude-and-conclusion" ,prelude-and-conclusion ,interp-x86-0)
      ))
